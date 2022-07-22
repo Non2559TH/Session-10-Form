6e67389d251797dc010b0cbd6fed41618af45b3a
Your Name <nonvrzoroklollollol@gmail.com>
2022-07-22 13:55:47 +0700

    first commit

---
+++ Session 10 .php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>work</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" 
+    crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@100&display=swap" rel="stylesheet">
+    <style type="text/css">
+        body{
+            font-family: 'Prompt', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form name="form1" action="#" method="post" enctype="multipart/formdata">
+<div class="container ">
+  <div class="row">
+    <div class="col">
+    <label for="#" class="form-label">คำนำหน้า</label>
+    <select class="form-select" aria-label="Default select example" id="txt_fname">
+  <option selected>คำนำหน้า</option>
+  <option value="1">นาย</option>
+  <option value="2">นาง</option>
+  <option value="3">นางสาว</option>
+</select>
+    </div>
+    <div class="col">
+    <label for="#" class="form-label">ชื่อ</label>
+      <input class="form-control" type="text" id="txt_name"placeholder="ชื่อ">
+    </div>
+    <div class="col">
+    <label for="#" class="form-label">นามสกุล</label>
+      <input class="form-control" type="text" id="txt_Lname"placeholder="นามสกุล">
+    </div>
+  </div>
+</div>
+</form>
+<?php
+
+?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" 
+    rossorigin="anonymous"></script>
+</body>
+</html>
